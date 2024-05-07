cc3831005348009822a648388c5402692926b5a9
Noelia <abascalcabreran@gmail.com>
2024-05-07 10:39:35 +0200

    form style

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -11,32 +11,34 @@ function Filter({ onChangeInput, onChangeSelect }) {
   };
 
   return (
-    <div>
+    <>
       <h3 className="titlesFilter">Filters:</h3>
-      <label className="label" htmlFor="name">
-        Search by country:{' '}
-      </label>
-      <input
-        className="input"
-        id="name"
-        type="text"
-        placeholder="Country..."
-        onChange={handleInput}
-      />
-      <label className="label" htmlFor="continent">
-        Search by continent:{' '}
-      </label>
-      <select id="continent" type="text" onChange={handleSelect}>
-        <option value="All">All</option>
-        <option value="Africa">Africa</option>
-        <option value="Antarctica">Antarctica</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="North America">North America</option>
-        <option value="Oceania">Oceania</option>
-        <option value="South America">South America</option>
-      </select>
-    </div>
+      <form className="form">
+        <label className="label" htmlFor="name">
+          Country:{' '}
+        </label>
+        <input
+          className="input"
+          id="name"
+          type="text"
+          placeholder="Country..."
+          onChange={handleInput}
+        />
+        <label className="label" htmlFor="continent">
+          Continent:{' '}
+        </label>
+        <select className="input" id="continent" type="text" onChange={handleSelect}>
+          <option value="All">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Antarctica">Antarctica</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="North America">North America</option>
+          <option value="Oceania">Oceania</option>
+          <option value="South America">South America</option>
+        </select>
+      </form>
+    </>
   );
 }
 
